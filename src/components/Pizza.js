04c7a94dba980ead3ebf66f577b871b12698a94c
Adam Finch <atyreefinch@gmail.com>
2019-11-25 16:33:42 -0500

    Done.

--- src/components/Pizza.js
+++ src/components/Pizza.js
@@ -1,12 +1,12 @@
 import React from "react"
 
-const Pizza = () => {
+const Pizza = (props) => {
   return(
     <tr>
-      <td>{"Replace Me With Pizza Topping"}</td>
-      <td>{"Replace Me With Pizza Size"}</td>
-      <td>{"Replace Me With Vegatarian"}</td>
-      <td><button type="button" className="btn btn-primary">Edit Pizza</button></td>
+      <td>{props.pizzaObj.topping}</td>
+      <td>{props.pizzaObj.size}</td>
+      <td>{props.pizzaObj.vegetarian ? "Yes" : "Nope"}</td>
+      <td><button onClick={()=>props.addPizzaToForm(props.pizzaObj)} type="button" className="btn btn-primary">Edit Pizza</button></td>
     </tr>
   )
 }
